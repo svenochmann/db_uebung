10e9ade98f6bea474c848a21caa061a849a347e0
Sven Ochmann <sven.och@outlook.com>
2023-06-09 16:17:48 +0200

    update

--- db_project.py
+++ db_project.py
@@ -6,11 +6,16 @@ import time
 from machine import Pin, PWM, Timer
 import math
 
+def sinus_gen():
+    return None
+
+
 class Sampler():
     # Klassenvariablen
     tim = Timer(0)  
     samples = []    
-    conv = False    
+    conv = False   
+    adc = ADC(Pin(), atten=ADC.ATTN_11DB) 
 
     def __init__(self, fs):
         machine.freq(240000000)  # CPU-Frequenz einstellen
@@ -32,6 +37,12 @@ class Sampler():
             Sampler.pwm_pin.duty_u16(duty)  # Duty für das PWM-Signal einstellen
             yield  
 
+
+    def convAD(self, T):
+        #T Aufnahme zeit in sekunden
+        
+        yield
+
     def handler(self, gen):
         try:
             next(gen)  # Nächstes Sample aus dem Generator abrufen
@@ -44,9 +55,45 @@ class Sampler():
         self.conv = True
         Sampler.tim.init(mode=Timer.PERIODIC, freq=self.fs, callback=lambda t: self.handler(gen))
         # Timer initialisieren: periodischer Modus, Frequenz fs, Callback-Funktion verwenden
+    def startAD(self, T):
+        gen = self.convAD(T)
+
+        self.conv = True
+        #Converten
+
 
 if __name__ == "__main__":
     sampler = Sampler(5000)  # Sampler-Objekt mit Abtastfrequenz 5000 Hz erstellen
     #Aufgabe 4 B:
     #Folgend sollte noch der Code der die Sinus-töne erstellt dies wurde zeitlich nicht mehr geschaft.
+    
+
+
+
+
+    #Vorgabe uebung_5
+    import gc
+    gc.collect()
+    print ("Freier Speicher: {0}KiB".format(gc.mem_free()/1024))     fs =6000 #sample freq     
+    T = 5#record time in sec     
+    sampler=Sampler(fs)     
+    for wait in range(3,0,-1):         
+        print("Sampling starts in {0} seconds...".format(wait))         
+        time.sleep(1)     
+        sampler.startAD(T)     
+        print("Recording...")     
+        start_ticks = time.ticks_ms()     
+        sampler.startAD(T)     
+        while (sampler.conv):
+            pass     
+        end_ticks = time.ticks_ms()     
+        print("Sampling after T={0} sec. finished ({1} samples, {2}KiB).".format(time.ticks_diff(end_ticks,start_ticks)/1000,                                                                                              
+    len(Sampler.samples),                                                                             
+    len(Sampler.samples)*2/1024))         
+        while True:         
+            sampler.startDA()         
+            print("Playback...")         
+            while (sampler.conv):pass         
+            print("Done.")         
+            time.sleep(2)
     
